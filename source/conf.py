0e2a366054ded63ae1865da6b350b306831824af
sethgis <50513411+sethgis@users.noreply.github.com>
2021-08-17 23:38:55 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -73,9 +73,9 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
-    'logo_only': False,
+    'logo_only': True,
     'display_version': True,
-    'prev_next_buttons_location': "both",
+    'prev_next_buttons_location': "bottom",
     'style_external_links': False,
     'vcs_pageview_mode': '',
     'style_nav_header_background': 'green',
